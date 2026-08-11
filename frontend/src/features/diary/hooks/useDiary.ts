267084793a2c85f60152a3a6e9c1a3aec90faaed
smarttrioai-pixel <smarttrio.ai@gmail.com>
2026-08-11 20:16:23 +0530

    Delete frontend directory

--- frontend/src/features/diary/hooks/useDiary.ts
+++
@@ -1,8 +0,0 @@
-import { useMutation } from "@tanstack/react-query";
-import { diaryApi } from "@/features/diary/data/diaryApi";
-
-export function useGenerateDiary() {
-  return useMutation({
-    mutationFn: (tripId: string) => diaryApi.generateDiary(tripId),
-  });
-}
